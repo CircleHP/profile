0415aa8b273ae17528a9d5dd9ec6dfcebd74ce36
CircleHP <evmenovdv@gmail.com>
2020-07-29 22:22:50 +0300

    Add default boilerplate with cfgs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import 'assets/global.css';
+
+import Home from 'screens/Home';
+import Portfolio from 'screens/Portfolio';
+import Settings from 'screens/Settings';
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <Switch>
+            <Route to='/' render={() => <Home />} />
+            <Route to='/portfolio' render={() => <Portfolio />} />
+            <Route to='/settings' render={() => <Settings />} />
+        </Switch>
     );
-}
+};
 
 export default App;
