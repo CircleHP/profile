0f7a9b28e6384baf039a3919786cff52fb1654c5
CircleHP <evmenovdv@gmail.com>
2020-08-10 17:19:06 +0300

    Minor additions

--- src/screens/Home/Stats/Stats.js
+++ src/screens/Home/Stats/Stats.js
@@ -1,37 +1,43 @@
 import React from 'react';
 
-const Stats = ({ header, data, total }) => {
-    // const persents = (totalInvested / total) * 100;
+const Stats = ({ data, total }) => {
+    const headers = data.map((item) => item.industry);
+    const headersSet = new Set(headers);
 
-    // let totalInvested = 0;
-    // console.log(data.map((item) => summ += item.invested))
-    // console.log(persents)
-
-    const medicine = data.filter((item) => item.industry === 'medicine').map((total) => total.invested).reduce((accum, item) => { return accum + item});
-    const build = data.filter((item) => item.industry === 'build').map((total) => total.invested).reduce((accum, item) => { return accum + item});
-    const fin = data.filter((item) => item.industry === 'fin').map((total) => total.invested).reduce((accum, item) => { return accum + item});
-    const indX = data.filter((item) => item.industry === 'ind-x').map((total) => total.invested).reduce((accum, item) => { return accum + item});
-    const other = data.filter((item) => item.industry === 'other').map((total) => total.invested).reduce((accum, item) => { return accum + item});
+    const medicine = data.filter((item) => item.industry === 'medicine').map((total) => total.invested).reduce((accum, item) => { return accum + item });
+    const build = data.filter((item) => item.industry === 'build').map((total) => total.invested).reduce((accum, item) => { return accum + item });
+    const fin = data.filter((item) => item.industry === 'fin').map((total) => total.invested).reduce((accum, item) => { return accum + item });
+    const indX = data.filter((item) => item.industry === 'ind-x').map((total) => total.invested).reduce((accum, item) => { return accum + item });
+    const other = data.filter((item) => item.industry === 'other').map((total) => total.invested).reduce((accum, item) => { return accum + item });
 
     const persents = (summ) => {
         return (summ / total) * 100
-    }
-
-    console.log(medicine, build, fin, indX, other);
+    };
 
     return (
-        <div style={{ display: 'flex', width: '100%' }}>
-            <div style={{ width: `${persents(medicine)}%`, height: 30, background: 'black' }}>
+        <>
+            <div style={{ fontSize: 36, padding: '10px 30px', color: '#3C4368'}}>{total} SEK</div>
+            <div style={{ display: 'flex', width: '100%', padding: '0 30px', overflow: 'hidden' }}>
+                <div style={{ width: `${persents(medicine)}%`, height: 20, background: '#5B74FF', borderRadius: '6px 0 0 6px' }}>
+                </div>
+                <div style={{ width: `${persents(build)}%`, height: 20, background: '#34BFA3' }}>
+                </div>
+                <div style={{ width: `${persents(fin)}%`, height: 20, background: '#FD397A' }}>
+                </div>
+                <div style={{ width: `${persents(indX)}%`, height: 20, background: '#3D4465' }}>
+                </div>
+                <div style={{ width: `${persents(other)}%`, height: 20, background: '#A1A8C3', borderRadius: '0 6px 6px 0' }}>
+                </div>
             </div>
-            <div style={{ width: `${persents(build)}%`, height: 30, background: 'red' }}>
+
+            <div style={{display: 'flex', padding: '50px 30px'}}>
+                <div style={{ width: 20, height: 20, background: '#5B74FF', borderRadius: 4}} />
+                <div>Medicine</div>
+                <div>{medicine}</div>
             </div>
-            <div style={{ width: `${persents(fin)}%`, height: 30, background: 'yellow' }}>
-            </div>
-            <div style={{ width: `${persents(indX)}%`, height: 30, background: 'green' }}>
-            </div>
-            <div style={{ width: `${persents(other)}%`, height: 30, background: 'grey' }}>
-            </div>
-        </div>
+
+            {/* <CompanyItem background='' label='' total={totalInvested}/> */}
+        </>
     );
 };
 
