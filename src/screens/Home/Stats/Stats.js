d65eb8b14fad0289eafd9aa0acb7c741f9eda5e6
Anara23 <anarikrulit@mail.ru>
2020-08-13 21:35:13 +0200

    added code to Stats to show morw information what companies the user invested in. Need to split the words with <,> (Företag 1 and others)

--- src/screens/Home/Stats/Stats.js
+++ src/screens/Home/Stats/Stats.js
@@ -12,7 +12,14 @@ const Stats = ({ data, total }) => {
     const indX = data.filter((item) => item.industry === 'ind-x').map((total) => total.invested).reduce((accum, item) => { return accum + item });
     const other = data.filter((item) => item.industry === 'other').map((total) => total.invested).reduce((accum, item) => { return accum + item });
 
-    const persents = (summ) => {
+    const medicineCompanyName = data.filter((item) => item.industry === 'medicine').map((total) => total.companyName).reduce((accum, item) => { return accum + item});
+    const buildCompanyName = data.filter((item) => item.industry === 'build').map((total) => total.companyName).reduce((accum, item) => { return accum + item });
+    const finCompanyName = data.filter((item) => item.industry === 'fin').map((total) => total.companyName).reduce((accum, item) => { return accum + item });
+    const indXCompanyName = data.filter((item) => item.industry === 'ind-x').map((total) => total.companyName).reduce((accum, item) => { return accum + item });
+    const otherCompanyName = data.filter((item) => item.industry === 'other').map((total) => total.companyName).reduce((accum, item) => { return accum + item });
+
+
+    const percents = (summ) => {
         return (summ / total) * 100
     };
 
@@ -28,41 +35,41 @@ const Stats = ({ data, total }) => {
             <div className='total-invested'>{total} SEK <UpdateTimestamp /></div>
             
             <div className='stats-bar'>
-                <div style={{ width: `${persents(medicine)}%`, height: 20, background: '#5B74FF', borderRadius: '6px 0 0 6px' }}>
+                <div style={{ width: `${percents(medicine)}%`, height: 20, background: '#5B74FF', borderRadius: '6px 0 0 6px' }}>
                 </div>
-                <div style={{ width: `${persents(build)}%`, height: 20, background: '#34BFA3' }}>
+                <div style={{ width: `${percents(build)}%`, height: 20, background: '#34BFA3' }}>
                 </div>
-                <div style={{ width: `${persents(fin)}%`, height: 20, background: '#FD397A' }}>
+                <div style={{ width: `${percents(fin)}%`, height: 20, background: '#FD397A' }}>
                 </div>
-                <div style={{ width: `${persents(indX)}%`, height: 20, background: '#3D4465' }}>
+                <div style={{ width: `${percents(indX)}%`, height: 20, background: '#3D4465' }}>
                 </div>
-                <div style={{ width: `${persents(other)}%`, height: 20, background: '#A1A8C3', borderRadius: '0 6px 6px 0' }}>
+                <div style={{ width: `${percents(other)}%`, height: 20, background: '#A1A8C3', borderRadius: '0 6px 6px 0' }}>
                 </div>
             </div>
 
             <div className='stats-style'>
                 <div style={{ width: 20, height: 20, background: '#5B74FF', borderRadius: 4}} />
-                <div>Medtech</div>
+                <div>Medtech <p>{medicineCompanyName}</p></div>
                 <div>{medicine} SEK</div>
             </div>
             <div className='stats-style'>
                 <div style={{ width: 20, height: 20, background: '#34BFA3', borderRadius: 4}} />
-                <div>Byggsektorn</div>
+                <div>Byggsektorn <p>{buildCompanyName}</p></div>
                 <div>{build} SEK</div>
             </div>
             <div className='stats-style'>
                 <div style={{ width: 20, height: 20, background: '#FD397A', borderRadius: 4}} />
-                <div>Fintech</div>
+                <div>Fintech <p>{finCompanyName}</p></div>
                 <div>{fin} SEK</div>
             </div>
             <div className='stats-style'>
                 <div style={{ width: 20, height: 20, background: '#3D4465', borderRadius: 4}} />
-                <div>Industri X</div>
+                <div>Industri X <p>{indXCompanyName}</p></div>
                 <div>{indX} SEK</div>
             </div>
             <div className='stats-style'>
                 <div style={{ width: 20, height: 20, background: '#A1A8C3', borderRadius: 4}} />
-                <div>Övrigt</div>
+                <div>Övrigt <p>{otherCompanyName}</p></div>
                 <div>{other} SEK</div>
             </div>
             
